11540d3f318ca84bf54b605b1376e28b397d48db
Bhagirath Makwana <83909096+Bhagi-developer@users.noreply.github.com>
2022-04-09 12:01:04 +0530

    Update screening.py

--- screening.py
+++ screening.py
@@ -36,3 +36,142 @@ plt.subplot(the_grid[0, 1], aspect=1, title='CATEGORY DISTRIBUTION')
 
 source_pie = plt.pie(targetCounts, labels=targetLabels, autopct='%1.1f%%', shadow=True, colors=colors)
 plt.show()
+
+""" here the diagram will show category distribution in a circle diagram"""
+
+#foolowing code cleans our resume dataset (line till 54)
+import re
+def cleanResume(resumeText):
+    resumeText = re.sub('http\S+\s*', ' ', resumeText)  # remove URLs
+    resumeText = re.sub('RT|cc', ' ', resumeText)  # remove RT and cc
+    resumeText = re.sub('#\S+', '', resumeText)  # remove hashtags
+    resumeText = re.sub('@\S+', '  ', resumeText)  # remove mentions
+    resumeText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', resumeText)  # remove punctuations
+    resumeText = re.sub(r'[^\x00-\x7f]',r' ', resumeText) 
+    resumeText = re.sub('\s+', ' ', resumeText)  # remove extra whitespace
+    return resumeText
+    
+resumeDataSet['cleaned_resume'] = resumeDataSet.Resume.apply(lambda x: cleanResume(x))
+
+
+
+import nltk
+from nltk.corpus import stopwords
+import string
+from wordcloud import WordCloud
+
+oneSetOfStopWords = set(stopwords.words('english')+['``',"''"])
+totalWords =[]
+Sentences = resumeDataSet['Resume'].values
+cleanedSentences = ""
+for i in range(0,160):
+    cleanedText = cleanResume(Sentences[i])
+    cleanedSentences += cleanedText
+    requiredWords = nltk.word_tokenize(cleanedText)
+    for word in requiredWords:
+        if word not in oneSetOfStopWords and word not in string.punctuation:
+            totalWords.append(word)
+    
+wordfreqdist = nltk.FreqDist(totalWords)
+mostcommon = wordfreqdist.most_common(50)
+print(mostcommon)
+
+wc = WordCloud().generate(cleanedSentences)
+plt.figure(figsize=(15,15))
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+"""
+output of above code (from 58)
+[('Details', 484), ('Exprience', 446), ('months', 376), ('company', 330), ('description', 310), ('1', 290), ('year', 232), 
+('January', 216), ('Less', 204), ('Data', 200), ('data', 192), ('Skill', 166), ('Maharashtra', 166), ('6', 164), ('Python', 156),
+('Science', 154), ('I', 146), ('Education', 142), ('College', 140), ('The', 126), ('project', 126), ('like', 126), ('Project', 124),
+('Learning', 116), ('India', 114), ('Machine', 112), ('University', 112), ('Web', 106), ('using', 104), ('monthsCompany', 102), ('B', 98), 
+('C', 98), ('SQL', 96), ('time', 92), ('learning', 90), ('Mumbai', 90), ('Pune', 90), ('Arts', 90), ('A', 84), ('application', 84), ('Engineering', 78),
+('24', 76), ('various', 76), ('Software', 76), ('Responsibilities', 76), ('Nagpur', 76), ('development', 74), ('Management', 74), ('projects', 74),
+('Technologies', 72)]
+"""
+
+#following code will convert words  into categorical values
+from sklearn.preprocessing import LabelEncoder
+
+var_mod = ['Category']
+le = LabelEncoder()
+for i in var_mod:
+    resumeDataSet[i] = le.fit_transform(resumeDataSet[i])
+    
+
+    
+  
+ # now we will train our model for Resume Screening 
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import hstack
+
+requiredText = resumeDataSet['cleaned_resume'].values
+requiredTarget = resumeDataSet['Category'].values
+
+word_vectorizer = TfidfVectorizer(
+    sublinear_tf=True,
+    stop_words='english',
+    max_features=1500)
+word_vectorizer.fit(requiredText)
+WordFeatures = word_vectorizer.transform(requiredText)
+
+print ("Feature completed .....")
+
+X_train,X_test,y_train,y_test = train_test_split(WordFeatures,requiredTarget,random_state=0, test_size=0.2)
+print(X_train.shape)
+print(X_test.shape)
+
+clf = OneVsRestClassifier(KNeighborsClassifier())
+clf.fit(X_train, y_train)
+prediction = clf.predict(X_test)
+print('Accuracy of KNeighbors Classifier on training set: {:.2f}'.format(clf.score(X_train, y_train)))
+print('Accuracy of KNeighbors Classifier on test set: {:.2f}'.format(clf.score(X_test, y_test)))
+
+print("\n Classification report for classifier %s:\n%s\n" % (clf, metrics.classification_report(y_test, prediction)))
+
+
+"""
+output
+Accuracy of KNeighbors Classifier on training set: 0.99
+Accuracy of KNeighbors Classifier on test set: 0.99
+
+ Classification report for classifier OneVsRestClassifier(estimator=KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+           metric_params=None, n_jobs=None, n_neighbors=5, p=2,
+           weights='uniform'),
+          n_jobs=None):
+              precision    recall  f1-score   support
+
+           0       1.00      1.00      1.00         3
+           1       1.00      1.00      1.00         3
+           2       1.00      0.80      0.89         5
+           3       1.00      1.00      1.00         9
+           4       1.00      1.00      1.00         6
+           5       0.83      1.00      0.91         5
+           6       1.00      1.00      1.00         9
+           7       1.00      1.00      1.00         7
+           8       1.00      0.91      0.95        11
+           9       1.00      1.00      1.00         9
+          10       1.00      1.00      1.00         8
+          11       0.90      1.00      0.95         9
+          12       1.00      1.00      1.00         5
+          13       1.00      1.00      1.00         9
+          14       1.00      1.00      1.00         7
+          15       1.00      1.00      1.00        19
+          16       1.00      1.00      1.00         3
+          17       1.00      1.00      1.00         4
+          18       1.00      1.00      1.00         5
+          19       1.00      1.00      1.00         6
+          20       1.00      1.00      1.00        11
+          21       1.00      1.00      1.00         4
+          22       1.00      1.00      1.00        13
+          23       1.00      1.00      1.00        15
+          24       1.00      1.00      1.00         8
+
+   micro avg       0.99      0.99      0.99       193
+   macro avg       0.99      0.99      0.99       193
+weighted avg       0.99      0.99      0.99       193
+"""
