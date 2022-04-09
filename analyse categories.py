57d4b9e5d687e0e9e3a86f56d44735c3bba79735
Bhagirath Makwana <83909096+Bhagi-developer@users.noreply.github.com>
2022-04-09 11:45:32 +0530

    Create analyse categories.py

---
+++ analyse categories.py
@@ -0,0 +1,38 @@
+print ("Displaying the distinct categories of resume -")
+print (resumeDataSet['Category'].unique())
+
+"""
+output
+Displaying the distinct categories of resume and the number of records belonging to each category -
+Java Developer               84
+Testing                      70
+DevOps Engineer              55
+Python Developer             48
+Web Designing                45
+HR                           44
+Hadoop                       42
+Mechanical Engineer          40
+Sales                        40
+ETL Developer                40
+Blockchain                   40
+Operations Manager           40
+Data Science                 40
+Arts                         36
+Database                     33
+Electrical Engineering       30
+Health and fitness           30
+PMO                          30
+DotNet Developer             28
+Business Analyst             28
+Automation Testing           26
+Network Security Engineer    25
+SAP Developer                24
+Civil Engineer               24
+Advocate                     20
+Name: Category, dtype: int64
+"""
+
+import seaborn as sns
+plt.figure(figsize=(15,15))
+plt.xticks(rotation=90)
+sns.countplot(y="Category", data=resumeDataSet)
